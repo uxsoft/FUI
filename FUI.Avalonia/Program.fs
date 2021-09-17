e3b9cdafb9add009105dfdaabd6170a2ace07906
Jan Dryk <jan.dryk@merck.com>
2021-09-17 23:37:28 +0200

    add rest of files omitted by git

---
+++ FUI.Avalonia/Program.fs
@@ -0,0 +1,34 @@
+﻿module FUI.Avalonia.Program
+
+open Avalonia
+open Avalonia.Themes.Fluent
+open Avalonia.Controls.ApplicationLifetimes
+open FUI.Avalonia.DSL
+
+let createMainWindow () =    
+    Window {
+        title "Counter Example"
+        height 400.
+        width 400.
+        
+        Counter.view ()
+    }
+        
+type App() =
+    inherit Application()
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme(baseUri = null, Mode = FluentThemeMode.Light))
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->            
+            desktopLifetime.MainWindow <- createMainWindow()
+        | _ -> ()
+    
+[<EntryPoint>]
+let main(args: string[]) =
+    AppBuilder
+        .Configure<App>()
+        .UsePlatformDetect()
+        .UseSkia()
+        .StartWithClassicDesktopLifetime(args)
