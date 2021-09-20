e681cbdfe4adbfd377209263969fd0825637dbab
github-action-benchmark <github@users.noreply.github.com>
2021-09-20 08:22:31 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 62c91eda6fc7aedfc95d1d36eed95f2200343939

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632125640538,
+  "lastUpdate": 1632126151936,
   "repoUrl": "https://github.com/uxsoft/FUI",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -26,6 +26,32 @@ window.BENCHMARK_DATA = {
             "value": 17936.679074074073,
             "unit": "ns",
             "range": "± 749.5471502816567"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "committer": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "id": "62c91eda6fc7aedfc95d1d36eed95f2200343939",
+          "message": "Simplified change",
+          "timestamp": "2021-09-19T21:51:37Z",
+          "url": "https://github.com/uxsoft/FUI/pull/5/commits/62c91eda6fc7aedfc95d1d36eed95f2200343939"
+        },
+        "date": 1632126151089,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FUI.Benchmarks.Benchmark+Benchmarks.CartesianProduct",
+            "value": 14914.306060606046,
+            "unit": "ns",
+            "range": "± 2509.203856577399"
           }
         ]
       }
