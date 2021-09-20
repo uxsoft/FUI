653a1f63a7c5d5139ba292f4928d397462297fff
github-action-benchmark <github@users.noreply.github.com>
2021-09-20 08:14:00 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 74aef6174417c6110e8bf2b355835aedff2a58ea

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632125640538,
+  "repoUrl": "https://github.com/uxsoft/FUI",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "committer": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "id": "74aef6174417c6110e8bf2b355835aedff2a58ea",
+          "message": "Simplified change",
+          "timestamp": "2021-09-19T21:51:37Z",
+          "url": "https://github.com/uxsoft/FUI/pull/5/commits/74aef6174417c6110e8bf2b355835aedff2a58ea"
+        },
+        "date": 1632125639950,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FUI.Benchmarks.Benchmark+Benchmarks.CartesianProduct",
+            "value": 17936.679074074073,
+            "unit": "ns",
+            "range": "± 749.5471502816567"
+          }
+        ]
+      }
+    ]
+  }
+}
