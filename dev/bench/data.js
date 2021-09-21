92d4102bc9dd559bca143443e025e9c04d1b11c1
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 06:26:34 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 8306bcc8d5b612c6b3bba71138b056ce7f0e90d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632126151936,
+  "lastUpdate": 1632205594506,
   "repoUrl": "https://github.com/uxsoft/FUI",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -52,6 +52,50 @@ window.BENCHMARK_DATA = {
             "value": 14914.306060606046,
             "unit": "ns",
             "range": "± 2509.203856577399"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "committer": {
+            "name": "uxsoft",
+            "username": "uxsoft"
+          },
+          "id": "8306bcc8d5b612c6b3bba71138b056ce7f0e90d6",
+          "message": "Simplified change",
+          "timestamp": "2021-09-19T21:51:37Z",
+          "url": "https://github.com/uxsoft/FUI/pull/5/commits/8306bcc8d5b612c6b3bba71138b056ce7f0e90d6"
+        },
+        "date": 1632205593606,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FUI.Benchmarks.CartesianProduct",
+            "value": 15425.816219075521,
+            "unit": "ns",
+            "range": "± 286.62929781278706"
+          },
+          {
+            "name": "FUI.Benchmarks.CounterUI",
+            "value": 1404810.7681749132,
+            "unit": "ns",
+            "range": "± 45890.63468741909"
+          },
+          {
+            "name": "FUI.Benchmarks.CartesianProduct",
+            "value": 17138.72298969075,
+            "unit": "ns",
+            "range": "± 1661.1308799348894"
+          },
+          {
+            "name": "FUI.Benchmarks.CounterUI",
+            "value": 1277789.1197826085,
+            "unit": "ns",
+            "range": "± 48591.369853914395"
           }
         ]
       }
