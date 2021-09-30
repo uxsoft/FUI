90b03960019574a826e41e1d6075891089d73f34
Jan Dryk <me@uxsoft.cz>
2021-09-30 15:42:56 +0200

    calendar components ranges

--- FUI.Avalonia/DSL/Components/DataValidationErrors.fs
+++ FUI.Avalonia/DSL/Components/DataValidationErrors.fs
@@ -1,21 +1,12 @@
 module FUI.Avalonia.DataValidationErrors
 
-    open System.Collections.Generic
-    open Avalonia.Controls.Templates
-    open Avalonia.Controls
-    open Avalonia.FuncUI.Types
-    open Avalonia.FuncUI.Builder
+open Avalonia.Controls.Templates
+open Avalonia.Controls
+open FUI.Avalonia.Types
     
-    let create (attrs: IAttr<DataValidationErrors> list): IView<DataValidationErrors> =
-        ViewBuilder.Create<DataValidationErrors>(attrs)
+type DataValidationErrorsBuilder<'t when 't :> DataValidationErrors and 't : equality>() =
+    inherit ContentControl.ContentControlBuilder<'t>()
     
-    type Control with
-        static member errors<'t when 't :> Control>(errors: IEnumerable<obj>) =
-            AttrBuilder<'t>.CreateProperty(DataValidationErrors.ErrorsProperty, errors, ValueNone)
-            
-        static member hasErrors<'t when 't :> Control>(hasErrors: bool) =
-            AttrBuilder<'t>.CreateProperty(DataValidationErrors.HasErrorsProperty, hasErrors, ValueNone)
-            
-    type DataValidationErrors with
-        static member errorTemplate<'t when 't :> DataValidationErrors>(template: IDataTemplate) =
-            AttrBuilder<'t>.CreateProperty(DataValidationErrors.ErrorTemplateProperty, template, ValueNone)
+    [<CustomOperation("errorTemplate")>]
+    member _.errorTemplate<'t>(x: AvaloniaNode<'t>, template: IDataTemplate) =
+        Types.dependencyProperty x DataValidationErrors.ErrorTemplateProperty template
--- FUI.Avalonia/DSL/Ranges/Slider.fs
+++ FUI.Avalonia/DSL/Ranges/Slider.fs
@@ -2,12 +2,10 @@
 
 open Avalonia.Collections
 open Avalonia.Controls
-open FUI.UiBuilder
 open FUI.Avalonia.RangeBase
 open Avalonia.Layout
-open Avalonia.FuncUI.Builder
 
-type SliderBuilder<'t when 't :> Slider>() =
+type SliderBuilder<'t when 't :> Slider and 't : equality>() =
     inherit RangeBaseBuilder<'t>()
 
     /// <summary>
@@ -15,26 +13,26 @@ type SliderBuilder<'t when 't :> Slider>() =
     /// </summary>
     [<CustomOperation("orientation")>]
     member _.orientation<'t>(x: Types.AvaloniaNode<'t>, value: Orientation) =
-        Types.dependencyProperty x<Orientation>(Slider.OrientationProperty, value, ValueNone) ]
+        Types.dependencyProperty x Slider.OrientationProperty value
 
     /// <summary>
     /// Sets a value that indicates whether the <see cref="Slider"/> automatically moves the <see cref="Thumb"/> to the closest tick mark.
     /// </summary>
     [<CustomOperation("isSnapToTickEnabled")>]
     member _.isSnapToTickEnabled<'t>(x: Types.AvaloniaNode<'t>, value: bool) =
-        Types.dependencyProperty x<bool>(Slider.IsSnapToTickEnabledProperty, value, ValueNone) ]
+        Types.dependencyProperty x Slider.IsSnapToTickEnabledProperty value
 
     /// <summary>
     /// Sets the interval between tick marks.
     /// </summary>
     [<CustomOperation("tickFrequency")>]
     member _.tickFrequency<'t>(x: Types.AvaloniaNode<'t>, value: float) =
-        Types.dependencyProperty x<float>(Slider.TickFrequencyProperty, value, ValueNone) ]
+        Types.dependencyProperty x Slider.TickFrequencyProperty value
 
     [<CustomOperation("tickPlacement")>]
     member _.tickPlacement<'t>(x: Types.AvaloniaNode<'t>, value: TickPlacement) =
-        Types.dependencyProperty x<TickPlacement>(Slider.TickPlacementProperty, value, ValueNone) ]
+        Types.dependencyProperty x Slider.TickPlacementProperty value
 
     [<CustomOperation("ticks")>]
     member _.ticks<'t>(x: Types.AvaloniaNode<'t>, value: float seq) =
-        Types.dependencyProperty x<float AvaloniaList>(Slider.TicksProperty, AvaloniaList value, ValueNone) ]
+        Types.dependencyProperty x Slider.TicksProperty (AvaloniaList value)
