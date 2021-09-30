5525b3c375346745d78c92a6f9a70519a5389328
Jan Dryk <me@uxsoft.cz>
2021-09-30 13:40:07 +0200

    Update data.js

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -13,32 +13,6 @@ window.BENCHMARK_DATA = {
             "name": "uxsoft",
             "username": "uxsoft"
           },
-          "id": "74aef6174417c6110e8bf2b355835aedff2a58ea",
-          "message": "Simplified change",
-          "timestamp": "2021-09-19T21:51:37Z",
-          "url": "https://github.com/uxsoft/FUI/pull/5/commits/74aef6174417c6110e8bf2b355835aedff2a58ea"
-        },
-        "date": 1632125639950,
-        "tool": "benchmarkdotnet",
-        "benches": [
-          {
-            "name": "FUI.Benchmarks.Benchmark+Benchmarks.CartesianProduct",
-            "value": 17936.679074074073,
-            "unit": "ns",
-            "range": "± 749.5471502816567"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "uxsoft",
-            "username": "uxsoft"
-          },
-          "committer": {
-            "name": "uxsoft",
-            "username": "uxsoft"
-          },
           "id": "62c91eda6fc7aedfc95d1d36eed95f2200343939",
           "message": "Simplified change",
           "timestamp": "2021-09-19T21:51:37Z",
