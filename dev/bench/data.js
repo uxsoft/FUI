25a88564d57942f2b9d58d6d33b34ccdd5cbd583
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 20:49:15 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 5794de606243ecabb9b30701d6424a6666a56a30

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633812498765,
+  "lastUpdate": 1633812555252,
   "repoUrl": "https://github.com/uxsoft/FUI",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -1408,6 +1408,54 @@ window.BENCHMARK_DATA = {
             "value": 1256502.1785714286,
             "unit": "ns",
             "range": "± 20411.77472591658"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@uxsoft.cz",
+            "name": "Jan Dryk",
+            "username": "uxsoft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5794de606243ecabb9b30701d6424a6666a56a30",
+          "message": "Add files via upload",
+          "timestamp": "2021-10-09T22:46:26+02:00",
+          "tree_id": "b712fc34f3002e33acedeb6c298b97ae7076ca19",
+          "url": "https://github.com/uxsoft/FUI/commit/5794de606243ecabb9b30701d6424a6666a56a30"
+        },
+        "date": 1633812554780,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FUI.Benchmarks.CartesianProduct",
+            "value": 15906.272875104632,
+            "unit": "ns",
+            "range": "± 131.7654178234438"
+          },
+          {
+            "name": "FUI.Benchmarks.CounterUI",
+            "value": 1098333.6143973214,
+            "unit": "ns",
+            "range": "± 30665.054599017978"
+          },
+          {
+            "name": "FUI.Benchmarks.CartesianProduct",
+            "value": 21919.721153846152,
+            "unit": "ns",
+            "range": "± 595.6161954838163"
+          },
+          {
+            "name": "FUI.Benchmarks.CounterUI",
+            "value": 1090582.8893333331,
+            "unit": "ns",
+            "range": "± 13563.810651802964"
           }
         ]
       }
